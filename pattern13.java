b0321dff06c03bfe06773ae884f996916656036a
manikandanmk7305 <manikandanmk7305@gmail.com>
2024-02-11 20:14:42 +0530

    mk

--- pattern13.java
+++ pattern13.java
@@ -1,29 +1,32 @@
-12.Write a program to print the following pattern
+
+13.Write a program to print the following pattern
 Example 1:
 
 Input: 
  
-4
+6
 
 Output:
 
-   1
-  12
- 123
-1234
+     6
+    65
+   654
+  6543
+ 65432
+654321
 
 code:
 import java.util.*;
 class Main{
   public static void main(String args[]){
     Scanner m=new Scanner(System.in);
-    int n,i,j,k;
+    int n;
     n=m.nextInt();
-    for(i=1;i<=n;i++){
-      for(j=n-1;j>=i;j--){
+    for(int i=n;i>=1;i--){
+      for(int j=i;j>1;j--){
         System.out.print(" ");
       }
-      for(k=1;k<=i;k++){
+      for(int k=n;k>=i;k--){
         System.out.print(k);
       }
       System.out.println();
